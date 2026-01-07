9743c127aa806a4f598a49bed6fef2acd8116052
Suvan77blr <sbumsrit61blr@gmail.com>
2026-01-07 13:47:00 +0530

    MAP: U2-05-DAXPY-Pthreads.c

---
+++ MAP-Multicore-Architecture-n-Programming/U2-Codes/05-Pthds-DAXPY-Loop.c
@@ -0,0 +1,59 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define THREAD_COUNT 4
+double *x, *y, a;
+int n;
+
+// Thread Function.
+void *daxpy(void* args) {
+    int id = *(int*)args + 1;
+    for(int i=0; i<n; i++) {
+        y[i] = a * x[i] + y[i];
+    }
+    return NULL;
+}
+
+int main(void) {
+    printf("Enter \'n\' : ");
+    scanf("%d", &n);
+
+    x = (double*) malloc(n * sizeof(double));
+    y = (double*) malloc(n * sizeof(double));
+
+    printf("Enter values of x[]: ");
+    for(int i=0; i<n; i++) {
+        scanf("%lf", (x+i));
+    }
+
+    printf("Enter values of y[]: ");
+    for(int i=0; i<n; i++) {
+        scanf("%lf", (y+i));
+    }
+
+    printf("Enter value of Scalar a: ");
+    scanf("%lf", &a);
+
+    // Creating the threads.
+    pthread_t threads[THREAD_COUNT];
+
+    for(int i=0; i<THREAD_COUNT; i++) {
+        pthread_create(&threads[i], NULL, daxpy, (int*) &i);
+    }
+
+    // Joining the created threads.
+    for(int i=0; i<n; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Resulting y[]:\n");
+    for (i = 0; i < n; i++)
+        printf("%lf ", y[i]);
+    printf("\n");
+
+    free(x);
+    free(y);
+    return 0;
+}
